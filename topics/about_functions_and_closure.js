b9d3294892b85f89e60cdc82a0f132a5b555afe2
Olle de Zwart <olle@endforward.nl>
2011-03-10 22:22:43 +0100

    Functions and Closure tests passing

--- topics/about_functions_and_closure.js
+++ topics/about_functions_and_closure.js
@@ -8,16 +8,16 @@ $(document).ready(function(){
 		function changeResult() {
 			// the ability to access a variables defined in the same scope as the function is known as 'closure'
 			result = "b";
-		};	
+		}	
 		changeResult();
-		equals(result, __, 'what is the value of result?');
+		equals(result, "b", 'what is the value of result?');
 	});
 	
 	test("assigning functions to variables", function() {
 		var triple = function(input) {
 			return input * 3;
 		};
-		equals(triple(4), __, 'what is triple 4?');		
+		equals(triple(4), 12, 'what is triple 4?');		
 	});
 	
 	test("self invoking functions", function() {		
@@ -26,13 +26,13 @@ $(document).ready(function(){
 		// self invoking functions are used to provide scoping and to alias variables
 		(function(pv) {
 			var secretValue = "password";
-			equals(pv, __, 'what is the value of pv?');
-			equals(typeof(secretValue), "__", "is secret value available in this context?");
-			equals(typeof(pv), "__", "is public value available in this context?");
+			equals(pv, "shared", 'what is the value of pv?');
+			equals(typeof(secretValue), "string", "is secret value available in this context?");
+			equals(typeof(pv), "string", "is public value available in this context?");
 		})(publicValue);
 		
-		equals(typeof(secretValue), "__", "is secret value available in this context?");
-		equals(typeof(publicValue), "__", "is public value available in this context?");
+		equals(typeof(secretValue), "undefined", "is secret value available in this context?");
+		equals(typeof(publicValue), "string", "is public value available in this context?");
 	});
 	
 	test("arguments array", function() {
@@ -40,8 +40,9 @@ $(document).ready(function(){
 			var total = 0;
 			for(var i = 0; i < arguments.length; i++) {
 				// complete the implementation of this method so that it returns the sum of its arguments
+        total = total + arguments[i];
 			}
-			// __
+      return total;
 		};
 	
 		equals(add(1,2,3,4,5), 15, "add 1,2,3,4,5");
